cb6f7947301b920b6b7a26f2fe6ab2014bcf836b
langrand <ludo.langrand@gmail.com>
2017-09-21 17:37:29 +0200

    rework app

--- app.js
+++ app.js
@@ -1,40 +1,18 @@
-var express = require('express');
-var app = express();
-var spotify = require('spotify');
-var spotifyPlaylist = require('spotify-playlist');
+var express = require('express')
+var app = express()
 
-app.use("/web", express.static(__dirname + "/web"));
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/web'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function (req, res) {
-    res.render('index.ejs', {error: false});
-});
+     res.render('index');
+})
 
-app.get('/playlist/:user/:playlist', function (req, res) {
-    var array = [];
-    var callback = function (err, result) {
-
-        var playlist = result.playlist.tracks;
-        if (playlist.length === 0) {
-            res.render('index.ejs', {error: true});
-        } else {
-            for (var i in playlist) {
-                array.push([
-                    playlist[i].number,
-                    playlist[i].name + ' - ' + playlist[i].artists,
-                    playlist[i].href,
-                    'Erreur'
-                ])
-            }
-            console.log('array = ',array);
-            res.render('playlist.ejs', {playlist: array});
-        }
-
-    }
-    spotifyPlaylist.playlist(req.params.user, req.params.playlist, callback);
-
-
-
-});
+app.get('/search/:user/:playlist', function (req, res) {
+     res.render('index');
+})
 
 
 app.listen(8080);
